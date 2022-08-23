e4f731ea0b7ce6b469997e12aab6929f3bd985ad
Labani Das <88694887+labanidas@users.noreply.github.com>
2022-08-23 18:57:54 +0530

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
   def index
 
 
-url = 'https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=a878768efc383e2dc35a3b44734f30ff'
+url = ENV["URL"]
 uri = URI(url)
 res = Net::HTTP.get_response(uri)
 
@@ -16,4 +16,3 @@ end
 
 
 # http://openweathermap.org/img/wn/10d@2x.png
-# https://api.openweathermap.org/data/2.5/weather?q=kolkata&appid=a878768efc383e2dc35a3b44734f30ff
